b333371d3e7c210edb540d1c47f93ec1ff491c95
noverd <dasfasru@gmail.com>
2024-04-14 20:04:35 +0300

    Done?

--- Lua14/Lua/Libraries/LuaSystemLibrary.cs
+++ Lua14/Lua/Libraries/LuaSystemLibrary.cs
@@ -1,3 +1,4 @@
+using HarmonyLib;
 using Lua14.Systems;
 using NLua;
 using Robust.Shared.GameObjects;
@@ -8,19 +9,26 @@ namespace Lua14.Lua.Libraries;
 
 public class LuaSystemLibrary : LuaLibrary
 {
+    [Dependency] private readonly HarmonyLib.Harmony _harmony = default!;
     [Dependency] private readonly EntityManager _entityManager = default!;
     private LuaSystem _luaSystem = null!;
     [Dependency] private readonly NLua.Lua _lua = default!;
     [Dependency] private readonly LuaLogger _logger = default!;
+    private static List<LuaFunction> _starters = [];
 
     public override void Initialize()
     {
-        if (!_entityManager.Initialized)
-            _logger.Warning("EntManager not inited");
-        var sys = _entityManager.System<SharedAppearanceSystem>();
+        _harmony.Patch(AccessTools.Method(AccessTools.TypeByName("Robust.Client.BaseClient"), "OnPlayerJoinedServer"),
+            postfix: new HarmonyMethod(Postfix));
     }
 
     public override string Name => "lua-sys";
+    
+    [LuaMethod("starter")]
+    public void AddStarter(LuaFunction starterFunc)
+    {
+        _starters.Add(starterFunc);
+    }
 
     [LuaMethod("addSystem")]
     public bool AddSystem(string name, LuaFunction updateFunc)
@@ -33,5 +41,13 @@ public class LuaSystemLibrary : LuaLibrary
     public bool RemoveSystem(string name)
     {
         return _luaSystem.TryRemoveLuaSystem(name);
+    }
+
+    static void Postfix()
+    {
+        foreach (var starter in _starters)
+        {
+            starter.Call();
+        }
     }
 }
