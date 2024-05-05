4a2ce4d615fd42bead3abe9e122604067175a37a
mynameundefined <52148806+mynameundefined@users.noreply.github.com>
2024-05-05 17:52:47 +0700

    entity lib draft

---
+++ Lua14/Lua/Libraries/EntityLibrary.cs
@@ -0,0 +1,24 @@
+﻿using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+
+namespace Lua14.Lua.Libraries;
+
+public sealed class EntityLibrary : LuaLibrary
+{
+    [Dependency] private readonly EntityManager _entity = default!;
+
+    public override string Name => "entity";
+
+    [LuaMethod("get")]
+    public IComponent[] Get(EntityUid uid, params Type[] types)
+    {
+        List<IComponent> components = [];
+
+        foreach (var type in types)
+        {
+            components.Add(_entity.GetComponent(uid, type));
+        }
+
+        return [.. components];
+    }
+}
